45b007597c711d36e6266db5959ddfbddc4e12c9
Chris Watts <cwatts@simplymeasured.com>
2016-11-02 16:47:03 -0700

    Add ember-simple-auth as a dependency

--- blueprints/ember-simple-auth-auth0/index.js
+++ blueprints/ember-simple-auth-auth0/index.js
@@ -1,9 +1,21 @@
 /*jshint node:true*/
+const RSVP = require('rsvp');
 
 module.exports = {
   description: '',
-  normalizeEntityName() {}, // no-op since we're just adding dependencies
+  normalizeEntityName() {
+  }, // no-op since we're just adding dependencies
   afterInstall() {
-    return this.addBowerPackagesToProject(['auth0-lock', 'auth0.js']);
+    return RSVP.all([
+      this.addPackageToProject('ember-simple-auth'),
+      this.addBowerPackagesToProject([
+        {
+          name: 'auth0-lock'
+        },
+        {
+          name: 'auth0.js'
+        }
+      ])
+    ]);
   }
 };
