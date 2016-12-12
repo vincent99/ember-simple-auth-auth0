44bd0c8cc71998a7d15f990490826cf8546e8793
Chris Watts <cwatts@simplymeasured.com>
2016-12-12 13:07:21 -0800

    Fix Ember.assign issue Remove ember-cli-commands

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 
+const assign = Ember.assign || Ember.merge;
+
 export default function startApp(attrs) {
   let application;
 
   // use defaults, but you can override
-  let attributes = Ember.assign({}, config.APP, attrs);
+  let attributes = assign({}, config.APP, attrs);
 
   Ember.run(() => {
     application = Application.create(attributes);
