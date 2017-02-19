731e049347075663c16198257da4255ff9082ef8
Chris Watts <chris.watts.t@gmail.com>
2017-02-18 17:11:48 -0800

    Add deprecation warnings

--- addon/authenticators/auth0-base.js
+++ addon/authenticators/auth0-base.js
@@ -23,14 +23,14 @@ export default BaseAuthenticator.extend({
     deprecate(
       'Should use "idToken" as the key for the authorization token instead of "jwt" key on the session data',
       isEmpty(jwt), {
-        id: 'ember-simple-auth-auth0.authenticators.auth0-lock.restore',
+        id: 'ember-simple-auth-auth0.authenticators.auth0-base.restore',
         until: 'v3.0.0',
       });
 
     deprecate(
       'Should use "idTokenPayload.exp" as the key for the expiration time instead of "exp" key on the session data',
       isEmpty(exp), {
-        id: 'ember-simple-auth-auth0.authenticators.auth0-lock.restore',
+        id: 'ember-simple-auth-auth0.authenticators.auth0-base.restore',
         until: 'v3.0.0',
       });
 
@@ -38,6 +38,13 @@ export default BaseAuthenticator.extend({
   },
 
   invalidate() {
+    deprecate(
+      'session.invalidate will no longer navigate to the auth0 logout url. Please call auth0.navigateToLogoutURL yourself when needed.',
+      false, {
+        id: 'ember-simple-auth-auth0.authenticators.auth0-base.invalidate',
+        until: 'v3.0.0',
+      });
+
     get(this, 'auth0').navigateToLogoutURL();
     return this._super(...arguments);
   },
