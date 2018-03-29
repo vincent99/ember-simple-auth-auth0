b47cee85a754ea9694293d9a8e3dfaaa075bc7d0
Xaser Acheron <xaser.88@gmail.com>
2018-03-29 09:53:44 -0500

    remove legacy auto-reject hack for passwordless lock; fixes session bug

--- addon/authenticators/auth0-lock-passwordless.js
+++ addon/authenticators/auth0-lock-passwordless.js
@@ -6,21 +6,11 @@ const {
   inject: {
     service
   },
-  RSVP,
-  typeOf,
 } = Ember;
 
 export default Auth0BaseAuthenticator.extend({
   auth0: service(),
-  authenticate(options, callback) {
-    if (typeOf(options) === 'function') {
-      callback = options;
-      options = {};
-    }
-
-    get(this, 'auth0').showPasswordlessLock(options).then(callback);
-    // NOTE: Always reject here so that the developer can use the proxied callback without being redirected to an authenticated state.
-    // Which is the default behavior.
-    return RSVP.reject();
+  authenticate(options) {
+    return get(this, 'auth0').showPasswordlessLock(options);
   },
 });
