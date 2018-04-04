72304f0fc5bf8df1c0a3e7dd181cebacf6ab0da8
Xaser Acheron <xaser.88@gmail.com>
2018-04-04 17:07:04 -0500

    rewrite createSessionDataObject to be properly Ember 2.4 compatible

--- addon/utils/create-session-data-object.js
+++ addon/utils/create-session-data-object.js
@@ -4,5 +4,9 @@ const assign = Ember.assign || Ember.merge;
 import now from '../utils/now';
 
 export default function createSessionDataObject(profile, tokenInfo) {
-  return assign({ issuedAt: now() }, tokenInfo, { profile });
+  // assign things one at a time so it's compatible with the Ember.merge fallback
+  const sessionData = { issuedAt: now() };
+  assign(sessionData, tokenInfo);
+  assign(sessionData, { profile });
+  return sessionData;
 }
