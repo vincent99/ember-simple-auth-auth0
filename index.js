f1320cd763037d21e6574fd613da1cb7f48e3d30
Miguel Andrade <jmandrade0@gmail.com>
2018-06-25 18:08:11 +0100

    keep add webpack plugin

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const webpack = require('webpack');
+
 module.exports = {
-  name: 'ember-simple-auth-auth0'
+  name: 'ember-simple-auth-auth0',
+  options: {
+    autoImport:{
+      webpack: {
+        plugins: [
+          new webpack.DefinePlugin({
+            'process.env': {
+              NODE_ENV: JSON.stringify(EmberAddon.env())
+            }
+          })
+        ]
+      }
+    }
+  }
 };
